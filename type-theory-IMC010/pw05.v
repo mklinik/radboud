4549054ca5429769d579f76909274fbd515d5da7
Markus Klinik <markus.klinik@gmx.de>
2012-10-09 16:48:24 +0200

    tt: pw05.v

--- type-theory-IMC010/pw05.v
+++ type-theory-IMC010/pw05.v
@@ -84,7 +84,10 @@ Qed.
 Theorem evenss : forall n:nat , (even n) -> (even (S (S n))).
 
 Proof.
-exact evenSS.
+intro n.
+intro H.
+apply evenSS.
+exact H.
 Qed.
 
 (*
@@ -103,13 +106,73 @@ Inductive le (n:nat) : nat -> Prop :=
 | le_n : le n n
 | le_S : forall m:nat , le n m -> le n (S m).
 
-Check (le O O).
-Check (le_n O).
-Check (le_n O).
-Check (le_S O O (le_n O)).
-Definition zero_smaller_than_one := (le_S O O (le_n O)).
+
+Check (le 0).
+Check (le 0 0). (* a type, which can be regarded as a
+  proposition. We don't know whether the proposition
+  is true or false yet. *)
+Check (le_n 0). (* a value of the above type, which can
+  be regarded as a proof that the above proposition
+  is true. *)
+Check (le 5 10).
+Check (le_n 100).
+Check (le_S 0 0 (le_n 0)).
+Definition zero_smaller_than_one := (le_S 0 0 (le_n 0)).
 Check zero_smaller_than_one.
 Check (le_n 7).
+
+(* We claim that zero is smaller than two.
+  This is equivalent to giving a declaration of an
+  identifier with the appropriate type.
+
+  The proof of the claim is to give a value that has
+  exactly this type.
+*)
+Lemma zero_smaller_than_two : le 0 (S (S 0)).
+Proof.
+apply le_S.
+apply le_S.
+apply le_n.
+Qed.
+
+(* what did we just do?
+to prove: the type le 0 (S (S 0)) is inhabited
+
+  last step in the proof: this is an axiom
+le_n 0 : le 0 0
+  because we know now that le_n is of type le 0 0, we can give this value as
+  the second argument of the constructor of le_S.
+le_S 0 (le_n 0) : le 0 (S 0)
+  We have just proved that the *value* le_S 0 (le_n 0) is of *type* le 0 (S 0).
+  As the last step, we apply the constructor le_S again.
+le_S (S 0) (le_S 0 (le_n 0) : le 0 (S (S 0))
+*)
+
+
+(* Question: can we give a data-type "ancestor"? *)
+Inductive ancestor (n:nat) : nat -> Prop :=
+    (* the successor of a number is an ancestor of that number *)
+  | anc_S : ancestor n (S n)
+    (* the ancestor of the sucessor of n is also an ancestor of n*)
+  | anc_anc : forall m:nat , ancestor n m -> ancestor n (S m)
+  .
+
+Check ancestor 100 0.
+Check anc_S 0.
+
+Check ancestor 1 0.
+Check anc_anc 0 0.
+
+(*
+Lemma no_number_is_its_own_ancestor : forall m:nat , not (ancestor m m).
+Proof.
+unfold not.
+intro m.
+elim m.
+intro.
+
+Qed.
+*)
 
 (* examples: sorted *)
 
@@ -120,8 +183,11 @@ Inductive natlist : Set :=
 
 (* an inductive predicate sorted *)
 Inductive sorted : natlist -> Prop :=
-| sorted0 : sorted nil
-| sorted1 : forall n:nat , sorted (cons n nil)
+| sorted0 : sorted nil (* the empty list is sorted *)
+| sorted1 : forall n:nat , sorted (cons n nil) (* the singleton list is sorted *)
+(* if n < h
+  and cons h t is sorted
+ then cons n (cons h t) is sorted. *)
 | sorted2 : forall n h:nat , forall t:natlist ,
             le n h -> sorted (cons h t) -> sorted (cons n (cons h t)).
 
@@ -144,6 +210,7 @@ Lemma one : forall n : nat, even n -> P n.
 
 Proof.
 intros n H.
+apply even_ind.
 inversion H.
 Abort.
 
@@ -168,8 +235,8 @@ Abort.
 (* exercise 1 *)
 Theorem even2 : (even 2 ).
 Proof.
-(*! proof *)
-
+apply evenSS.
+apply evenO.
 Qed.
 
 (* a few checks *)
@@ -184,23 +251,25 @@ Print even2.
    use inversion *)
 Theorem noteven1 : ~(even 1).
 Proof.
-(*! proof *)
-
+intro.
+inversion H.
 Qed.
 
 (* exercise 3
    you may want to use an earlier proved result *)
 Theorem even4 : even 4.
 Proof.
-(*! proof *)
-
+apply evenSS.
+apply even2.
 Qed.
 
 (* exercise 4 *)
 Theorem noteven3 : ~(even 3).
 Proof.
-(*! proof *)
-
+intro.
+inversion H.
+apply noteven1.
+assumption.
 Qed.
 
 (* an inductive definition of even and odd *)
@@ -225,37 +294,46 @@ Qed.
 (* exercise 5 *)
 Theorem ev2 : ev 2.
 Proof.
-(*! proof *)
-
+apply evS.
+apply odd1.
 Qed.
 
 (* exercise 6 *)
 Theorem notodd2 : ~ odd 2.
 Proof.
-(*! proof *)
-
+intro.
+inversion H.
+inversion H1.
+inversion H3.
 Qed.
 
 (* exercise 7
    use induction *)
 Theorem evorodd : forall n:nat, ev n \/ odd n.
 Proof.
-(*! proof *)
-
+intro.
+induction n.
+left.
+exact evO.
+inversion IHn.
+right.
+apply oddS.
+assumption.
+left.
+apply evS.
+assumption.
 Qed.
 
 (* exercise 8 *)
 Theorem zero_and_zero : le O O.
 Proof.
-(*! proof *)
-
+exact (le_n 0).
 Qed.
 
 (* exercise 9 *)
 Theorem zero_and_one  : le 0 1.
 Proof.
-(*! proof *)
-
+exact (le_S 0 0 zero_and_zero).
 Qed.
 
 (* some checks *)
@@ -265,32 +343,41 @@ Check zero_and_one.
 (* exercise 10 *)
 Theorem one_and_zero : ~ (le 1 0).
 Proof.
-(*! proof *)
-
+intro.
+inversion H.
 Qed.
 
 
 (* exercise 11 *)
 Theorem sortednil : sorted nil.
 Proof.
-(*! proof *)
-
+exact sorted0.
 Qed.
 
 
 (* exercise 12 *)
 Theorem sortedone : sorted (cons 0 nil).
 Proof.
-(*! proof *)
-
+Print sorted.
+exact (sorted1 0).
 Qed.
 
 (* exercise 13 *)
 Theorem sorted_one_two_three :
   sorted (cons 1 (cons 2 (cons 3 nil))).
 Proof.
-(*! proof *)
-
+Print sorted.
+apply sorted2.
+Lemma le_succ : forall n , le n (S n).
+Proof.
+intro.
+apply le_S.
+apply le_n.
+Qed.
+apply le_succ.
+apply sorted2.
+apply le_succ.
+apply sorted1.
 Qed.
 
 (* exercise 14 *)
@@ -299,8 +386,10 @@ Theorem sorted_tail :
   sorted (cons n l) ->
   sorted l.
 Proof.
-(*! proof *)
-
+intros.
+inversion H.
+exact sorted0.
+assumption.
 Qed.
 
 (* given for exercise 15
@@ -321,9 +410,15 @@ Inductive without_last (n:nat) : natlist -> natlist -> Prop :=
    use three clauses: for the empty list, for a list of one
    element, for a list of two or more elements *)
 
-Inductive palindrome : natlist -> Prop := (*! term *)
+Inductive palindrome : natlist -> Prop :=
+  | pali_nil : palindrome nil
+  | pali_one : forall n:nat, palindrome (cons n nil)
+  | pali_more : forall n:nat, forall l k:natlist,
+        without_last n k l -> palindrome l -> palindrome (cons n k)
   .
 
+  (* l ++  [n]   and  l is a palindrome, then    n : k      is a palindrome
+     |-- k --|                                n : l ++ [n]*)
 
 (*
 vim: filetype=coq
