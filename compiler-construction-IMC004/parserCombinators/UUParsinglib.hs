112c02d1f3de65f7bcddce8cfddb2d189274c3f1
Markus Klinik <markus.klinik@gmx.de>
2013-02-10 11:12:39 +0100

    left recursive nested parens

--- compiler-construction-IMC004/parserCombinators/UUParsinglib.hs
+++ compiler-construction-IMC004/parserCombinators/UUParsinglib.hs
@@ -13,8 +13,14 @@ parens =
   (\_ p _ q -> max (p+1) q)
     <$> (pSym '(') <*> parens <*> pSym ')' <*> parens
   <|> pure 0
--- parseParens = run parens "###(())(a)"
-parseParens = run parens "#"
+parseParens = run parensLeftRec "#(())((()))"
+-- parseParens = run parensLeftRec "#"
+
+parensLeftRec :: Parser Int
+parensLeftRec =
+  (\q _ p _ -> max (p+1) q)
+    <$> parens <*> (pSym '(') <*> parens <*> pSym ')'
+  <|> pure 0
 
 run p input = parse ((,) <$> p <*> pEnd) (createStr (LineCol 0 0) input)
 
