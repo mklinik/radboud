26e10e8fc76a458614a965f123a6736dabf79739
Markus Klinik <markus.klinik@gmx.de>
2013-02-12 19:29:30 +0100

    some lexing

--- compiler-construction-IMC004/src/Lexer.hs
+++ compiler-construction-IMC004/src/Lexer.hs
@@ -1,18 +1,53 @@
+{-# LANGUAGE  FlexibleContexts, NoMonomorphismRestriction #-}
 module Lexer where
 
 import Text.ParserCombinators.UU
 import Text.ParserCombinators.UU.BasicInstances
 import Text.ParserCombinators.UU.Derived
-import Text.ParserCombinators.UU.Utils hiding (pDigit)
+import Text.ParserCombinators.UU.Utils
 
 keywords =
   [ "Void"
-	, "Int"
-	, "Bool"
-	, "if"
-	, "else"
-	, "while"
-	, "return"
-	, "False"
-	, "True"
-	]
+  , "Int"
+  , "Bool"
+  , "if"
+  , "else"
+  , "while"
+  , "return"
+  , "False"
+  , "True"
+  ]
+
+punctuation =
+  [ '('
+  , ')'
+  ]
+
+operators =
+  [ '+'
+  , '-'
+  , '*'
+  , '/'
+  , '='
+  ]
+
+data Token
+  = Keyword String
+  | Punctuation Char
+  | Identifier String
+  | Number Integer
+  | Operator Char
+  deriving (Show)
+
+lexer :: Parser [Token]
+lexer = many (lexeme pTok)
+
+pTok :: Parser Token
+pTok =
+      (foldl1 (<|>) $ map (Punctuation <$>) $ map pSym punctuation)
+  <|> (foldl1 (<|>) $ map (Operator <$>) $ map pSym operators)
+  <|> (\i -> if i `elem` keywords then Keyword i else Identifier i) <$> pIdentifier
+  <|> Number <$> pInteger
+
+pIdentifier :: Parser String
+pIdentifier = ((:) <$> (pLetter <|> pSym '_') <*> pList1 (pDigit <|> pLetter <|> pSym '_'))
---
+++ compiler-construction-IMC004/src/Parser.hs
@@ -0,0 +1,6 @@
+module Parser where
+
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU.BasicInstances
+import Text.ParserCombinators.UU.Derived
+import Text.ParserCombinators.UU.Utils
