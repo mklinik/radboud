6eccb2e7cde2d78289ac77c0653f4858d8d53ee1
Markus Klinik <markus.klinik@gmx.de>
2013-03-03 18:50:07 +0100

    test left-associativity of (+) and (*)

--- compiler-construction-IMC004/test/Test.hs
+++ compiler-construction-IMC004/test/Test.hs
@@ -31,15 +31,31 @@ specs = do
 
     it "parses (*) with higher precedence than (:)" $
       parse pExpr "1 * 2 : []" `shouldBe` AstBinOp ":" (AstBinOp "*" (AstInteger 1) (AstInteger 2)) AstEmptyList
-
     it "parses (*) with higher precedence than (+)" $
       parse pExpr "1 + 2 * 3" `shouldBe` AstBinOp "+" (AstInteger 1) (AstBinOp "*" (AstInteger 2) (AstInteger 3))
-
     it "parses (/) with higher precedence than (-)" $
       parse pExpr "1 - 4 / 2" `shouldBe` AstBinOp "-" (AstInteger 1) (AstBinOp "/" (AstInteger 4) (AstInteger 2))
-
     it "parses (%) with higher precedence than (+)" $
       parse pExpr "1 + 2 % 0" `shouldBe` AstBinOp "+" (AstInteger 1) (AstBinOp "%" (AstInteger 2) (AstInteger 0))
+    it "parses (+) with higher precedence than (<)" $
+      parse pExpr "10 < 2 + 9" `shouldBe` AstBinOp "<" (AstInteger 10) (AstBinOp "+" (AstInteger 2) (AstInteger 9))
+
+    it "parses (&&) with higher precedence than (||)" $
+      parse pExpr "True || True && False" `shouldBe` AstBinOp "||" (AstBoolean True) (AstBinOp "&&" (AstBoolean True) (AstBoolean False))
+
+    it "parses (+) left-associative" $
+      parse pExpr "1 + 2 + 3 + 4" `shouldBe`
+        AstBinOp "+" (AstBinOp "+" (AstBinOp "+" (AstInteger 1)
+                                                 (AstInteger 2))
+                                   (AstInteger 3))
+                     (AstInteger 4)
+
+    it "parses (*) left-associative" $
+      parse pExpr "1 * 2 * 3 * 4" `shouldBe`
+        AstBinOp "*" (AstBinOp "*" (AstBinOp "*" (AstInteger 1)
+                                                 (AstInteger 2))
+                                   (AstInteger 3))
+                     (AstInteger 4)
 
 
 main :: IO ()
