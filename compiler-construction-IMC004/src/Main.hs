957d35f64d5b678341b5a66eac5cf95bc312a651
Markus Klinik <markus.klinik@gmx.de>
2013-04-07 11:06:57 +0200

    Main: only prettyprint inferred ast, dont' print environment

--- compiler-construction-IMC004/src/Main.hs
+++ compiler-construction-IMC004/src/Main.hs
@@ -93,7 +93,7 @@ typecheck ast =
   in
     case result of
     Left err -> Left $ show err
-    Right (env, ast2) -> Right $ TC.prettyprintGlobals env ++ prettyprint ast2
+    Right (_, ast2) -> Right $ prettyprint ast2
 
 cleanUp :: Options -> IO ()
 cleanUp opts = do
